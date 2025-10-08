4334361b0a0dd3b7ce6ae33a01c14ebaf24dc00c
xingsir12 <danilnek615@gmail.com>
2025-10-08 18:19:19 +0300

    Initial commit

---
+++ src/main/java/ru/xing/springcourse/petproject3rest/PetProject3RestApplication.java
@@ -0,0 +1,13 @@
+package ru.xing.springcourse.petproject3rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProject3RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProject3RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/xing/springcourse/petproject3rest/PetProject3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.xing.springcourse.petproject3rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProject3RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
