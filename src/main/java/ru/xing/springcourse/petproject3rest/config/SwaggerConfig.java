4f1e69fb1f9434e078bf2177428247943baa3b44
xingsir12 <167809727+xingsir12@users.noreply.github.com>
2025-10-28 02:30:42 +0300

    Update SwaggerConfig.java

--- src/main/java/ru/xing/springcourse/petproject3rest/config/SwaggerConfig.java
+++ src/main/java/ru/xing/springcourse/petproject3rest/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
