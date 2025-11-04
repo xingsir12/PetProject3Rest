180fca76e1b0f089f6eaea17c890311c2a88ad49
xingsir12 <danilnek615@gmail.com>
2025-11-05 00:50:22 +0300

    another changes

--- src/main/java/ru/xing/springcourse/petproject3rest/config/RedisConfig.java
+++ src/main/java/ru/xing/springcourse/petproject3rest/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package ru.xing.springcourse.petproject3rest.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class RedisConfig {
 
     @Bean
