a20e158ac670092e15adbaca316f59c2371f0a87
xingsir12 <danilnek615@gmail.com>
2025-11-05 00:58:04 +0300

    redis in application

--- src/main/java/ru/xing/springcourse/petproject3rest/PetProject3RestApplication.java
+++ src/main/java/ru/xing/springcourse/petproject3rest/PetProject3RestApplication.java
@@ -2,8 +2,13 @@ package ru.xing.springcourse.petproject3rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class
+})
 public class PetProject3RestApplication {
 
     public static void main(String[] args) {
